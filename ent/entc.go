5ea4bf86907da3038186dd159326181dcb87b455
seuno <toloruntotosho@gmail>
2022-10-09 13:25:35 +0100

    implementing oas and ogen

--- ent/entc.go
+++ ent/entc.go
@@ -6,20 +6,47 @@ package main
 import (
 	"log"
 
+	"ariga.io/ogent"
+	"entgo.io/contrib/entoas"
 	"entgo.io/ent/entc"
 	"entgo.io/ent/entc/gen"
-	"github.com/masseelch/elk"
+	"github.com/ogen-go/ogen"
 )
 
 func main() {
-	ex, err := elk.NewExtension(
-		elk.GenerateSpec("openapi.json"),
-		elk.GenerateHandlers(),
+	//ex, err := entoas.NewExtension()
+	//if err != nil {
+	//	log.Fatalf("creating entoas extension: %v", err)
+	//}
+	//err = entc.Generate("./schema", &gen.Config{}, entc.Extensions(ex))
+	//if err != nil {
+	//	log.Fatalf("running ent codegen: %v", err)
+	//}
+
+	spec := new(ogen.Spec)
+	oas, err := entoas.NewExtension(
+		entoas.Spec(spec),
+		entoas.Mutations(func(graph *gen.Graph, spec *ogen.Spec) error {
+			spec.AddPathItem("/db-health", ogen.NewPathItem().
+				SetDescription("Check the servers DB status").
+				SetGet(ogen.NewOperation().
+					SetOperationID("DBHealth").
+					SetSummary("Ping the database and report").
+					AddResponse("204", ogen.NewResponse().SetDescription("DB is reachable")).
+					AddResponse("503", ogen.NewResponse().SetDescription("DB is not reachable")),
+				),
+			)
+			return nil
+		}),
 	)
 	if err != nil {
-		log.Fatalf("creating elk extension: %v", err)
+		log.Fatalf("creating entoas extension: %v", err)
 	}
-	err = entc.Generate("./schema", &gen.Config{}, entc.Extensions(ex))
+	ogent, err := ogent.NewExtension(spec)
+	if err != nil {
+		log.Fatalf("creating ogent extension: %v", err)
+	}
+	err = entc.Generate("./schema", &gen.Config{}, entc.Extensions(ogent, oas))
 	if err != nil {
 		log.Fatalf("running ent codegen: %v", err)
 	}
--- main.go
+++ main.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	"GoEntFiberPokeman/ent"
-	elk "GoEntFiberPokeman/ent/http"
+	"GoEntFiberPokeman/ent/ogent"
 	"context"
+	"database/sql"
+	"entgo.io/ent/dialect"
+	entsql "entgo.io/ent/dialect/sql"
+	"flag"
 	"fmt"
-	"github.com/go-chi/chi/v5"
 	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
-	"go.uber.org/zap"
 	"log"
 	"net/http"
 )
+
+type handler struct {
+	*ogent.OgentHandler
+	db *sql.DB
+}
+
+func (h handler) DBHealth(_ context.Context) (ogent.DBHealthRes, error) {
+	if err := h.db.Ping(); err != nil {
+		return &ogent.DBHealthServiceUnavailable{}, nil
+	}
+	return &ogent.DBHealthNoContent{}, nil
+}
 
 func main() {
 	viper.SetConfigFile(".env")
@@ -22,33 +36,46 @@ func main() {
 		"host=%s port=%s user=%s dbname=%s password=%s",
 		viper.Get("DB_HOST"), viper.Get("DB_PORT"), viper.Get("DB_USER"), viper.Get("DB_NAME"), viper.Get("DB_PASSWORD"),
 	)
-	c, err := ent.Open("postgres", dsn)
+
+	var args struct {
+		Addr string
+		DSN  string
+	}
+
+	flag.StringVar(&args.Addr, "addr", ":8080", "http address to listen")
+	flag.StringVar(&args.DSN, "dsn", "file:ent?mode=memory&cache=shared&_fk=1", "dsn of database")
+	flag.Parse()
+
+	// Create ent client.
+	drv, err := entsql.Open(dialect.Postgres, dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client, err := ent.Open("postgres", dsn)
 	if err != nil {
 		log.Fatalf("failed opening connection to postgres: %v", err)
 	}
-	defer c.Close()
-	if err := c.Schema.Create(ctx); err != nil {
+	defer client.Close()
+	if err := client.Schema.Create(ctx); err != nil {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
 
 	// Run the auto migration tool.
-	if err := c.Schema.Create(context.Background()); err != nil {
+	if err := client.Schema.Create(context.Background()); err != nil {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
-	// Router and Logger.
-	r, l := chi.NewRouter(), zap.NewExample()
-	// Create the pet handler.
-	r.Route("/pets", func(r chi.Router) {
-		elk.NewPetHandler(c, l).Mount(r, elk.PetRoutes)
-	})
-	// Create the user handler.
-	r.Route("/users", func(r chi.Router) {
-		elk.NewUserHandler(c, l).Mount(r, elk.UserRoutes)
-	})
+
 	// Start listen to incoming requests.
-	fmt.Println("Server running")
+	fmt.Println("Server starting")
 	defer fmt.Println("Server stopped")
-	if err := http.ListenAndServe(":8080", r); err != nil {
+
+	srv, _ := ogent.NewServer(handler{
+		OgentHandler: ogent.NewOgentHandler(client),
+		db:           drv.DB(),
+	})
+
+	if err := http.ListenAndServe(args.Addr, srv); err != nil {
 		log.Fatal(err)
 	}
 }
